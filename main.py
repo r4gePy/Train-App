440e052a9f75f042418ba17533774e3a321bb3fc
Arsen Aganesov <44568470+r4gePy@users.noreply.github.com>
2019-04-05 23:25:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
+from prettytable import PrettyTable
 from route import Route
 from train import Train
 from station import Station
-
-train = Train("123", "123", 10)
-m = Route()
-m.create_route("stv", "moscow")
-m.add_point("rostov")
-m.add_point("voronezh")
-r = m.print_station()
-train.get_route(r)
-print(train.return_next_position())
-print(train.return_position())
-print(train.return_previous())
+from commands import *
